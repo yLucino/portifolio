a57da9f01a207c8ea30fb189763d4b0ec4818d7a
Luciano Chiodini <138342951+yLucino@users.noreply.github.com>
2024-08-23 23:20:47 -0300

    adjustment in code

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /* Typed Js */
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'DevQuest Student', 'React Framework'],
+    strings: ['Frontend Developer', 'DevQuest Student', 'React'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
